76f537e699f362411d81814ae6373b9f3713b50e
Helton M Leite <heltonml@gmail.com>
2024-02-07 14:11:33 -0300

    config: starting project spring + jasper

---
+++ src/main/java/com/jaspert_report/JaspertReportApplication.java
@@ -0,0 +1,13 @@
+package com.jaspert_report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaspertReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaspertReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaspert_report/JaspertReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaspert_report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaspertReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
